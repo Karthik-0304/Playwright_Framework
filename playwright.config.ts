40e91820e0d11c5c38cae2e04891c29c9b516b73
Karthik-0304 <karthik.sivakumar004@gmail.com>
2026-09-22 11:09:15 +0530

    Add Allure reporting

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,7 +25,8 @@ export default defineConfig({
 
     reporter: [
         ["list"],
-        ["html", { open: "never" }]
+        ["html", { open: "never" }],
+        ["allure-playwright"]
     ],
 
     use: {
